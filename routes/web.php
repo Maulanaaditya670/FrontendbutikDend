6991e1f277a3a9a64857cb0478cf7de77f26fe3b
Maulanaaditya670 <maulanaaditya670@gmail.com>
2024-06-03 22:26:31 +0700

    menambahkan login admin

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
+use illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginController;
 
-use Illuminate\Support\Facades\Route;
+Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [LoginController::class, 'login'])->name('login.post');
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/home', function () {
+    return view('home');
+})->name('home');
+
